e6cf3891af22ef2d6dd9f84d19128ec459c24092
Elijah <elijahcutler@gmail.com>
2020-02-01 13:14:39 -0500

    Continued work on Home formatting

--- mobile_app/src/screens/Home.tsx
+++ mobile_app/src/screens/Home.tsx
@@ -1,87 +1,123 @@
 import React, { Component } from 'react';
 import {
-  SafeAreaView,
-  StatusBar,
-  View,
-  Text,
+    SafeAreaView,
+    StatusBar,
+    View,
+    Text,
+    FlatList,
+    StyleSheet
 } from 'react-native';
 import { NavigationSwitchScreenProps } from "react-navigation";
 import MapView, { Region } from 'react-native-maps';
 import Geolocation from 'react-native-geolocation-service';
+import { Location } from '../lib/types';
 
 interface Props extends NavigationSwitchScreenProps { };
 
 interface State {
-  region?: Region;
-  currentPosition?: Geolocation.GeoPosition;
+    region?: Region;
+    currentPosition?: Geolocation.GeoPosition;
 }
 
-export default class SplashScreen extends Component<Props, State> {
-  static navigationOptions = () => {
-    return {
-      header: () => null
+const RECENT_LOCATIONS: Location[] = [
+    {
+        latitude: 38.922406,
+        longitude: -77.042154,
+        distanceFromUser: null,
+        title: "Songbyrd Bar DC"
+    },
+    {
+        latitude: 38.888420,
+        longitude: -77.022904,
+        distanceFromUser: null,
+        title: "Hirshhorn Art Gallery"
+    },
+    {
+        latitude: 38.907582,
+        longitude: -77.072351,
+        distanceFromUser: null,
+        title: "Georgetown University"
     }
-  }
+];
 
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.015,
-        longitudeDelta: 0.0121,
-      }
-    };
-  }
+export default class SplashScreen extends Component<Props, State> {
+    static navigationOptions = () => {
+        return {
+            header: () => null
+        }
+    }
 
-  componentDidMount() {
-    Geolocation.requestAuthorization();
-    Geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({
-          region: {
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          },
-          currentPosition: position
-        });
-      },
-      (error) => {
-        // See error code charts below.
-        console.log(error.code, error.message);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  }
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            region: {
+                latitude: 37.78825,
+                longitude: -122.4324,
+                latitudeDelta: 0.015,
+                longitudeDelta: 0.0121,
+            }
+        };
+    }
 
-  render() {
-    const { region } = this.state;
+    verifyIsDriver() {
 
-    return (
-      <>
-        <StatusBar barStyle="dark-content" />
-        <SafeAreaView style={{ flex: 1 }}>
-          <MapView
-            style={{ flex: 1 }}
-            showsUserLocation={true}
-            region={region || {
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            }} />
-            <View style={{flex: 0.5, backgroundColor: '#D95F76'}}>
-                <View style={{paddingLeft: '5%', paddingTop: '5%'}}>
-                <Text style={{color: 'white', fontSize: 26, fontWeight: "500"}}>
-                Plans for today?
-                </Text>
-                </View>
-            </View>
-        </SafeAreaView>
-      </>
-    );
-  }
+    }
+
+    componentDidMount() {
+        Geolocation.requestAuthorization();
+        Geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    region: {
+                        latitude: position.coords.latitude,
+                        longitude: position.coords.longitude,
+                        latitudeDelta: 0.015,
+                        longitudeDelta: 0.0121,
+                    },
+                    currentPosition: position
+                });
+            },
+            (error) => {
+                // See error code charts below.
+                console.log(error.code, error.message);
+            },
+            { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+        );
+    }
+
+    render() {
+        const { region } = this.state;
+
+        return (
+            <>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView style={{ flex: 1 }}>
+                    <MapView
+                        style={{ flex: 1 }}
+                        showsUserLocation={true}
+                        region={region || {
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.015,
+                            longitudeDelta: 0.0121,
+                        }} />
+                    <View style={{ flex: 0.5, backgroundColor: '#F3F3F3'}}>
+                            <Text style={{paddingLeft: '5%', paddingTop: '5%', color: '#D95F76', fontSize: 26, fontWeight: "600" }}>
+                                Plans for today?
+                            </Text>
+                        <View style={{flex: 1, justifyContent: "center"}}>
+                                <FlatList
+                                    data={RECENT_LOCATIONS}
+                                    renderItem={({ item }: { item: Location }) => 
+                                    <View><Text>{item.title}</Text></View>}
+                                    keyExtractor={(_: Location, i: number) => i.toString()}
+                                >
+                                </FlatList>
+                        </View>
+                        
+                    </View>
+                </SafeAreaView>
+            </>
+        );
+    }
 }
