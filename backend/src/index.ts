0a5e544e7df82881468b1c160eae595c67bf3b25
Kirin Patel <kirinpatel@gmail.com>
2020-01-31 21:42:56 -0500

    Created backend project

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import * as bodyparser from "body-parser";
+import cors from "cors";
+import express from "express";
+import { Application, Response } from "express";
+
+const PORT = process.env.port || 8080;
+
+const app = express() as Application;
+
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(bodyparser.json());
+app.use(cors());
+
+app.use("/", (_, res: Response) => {
+  res.status(200).send("Hello, Hoya!");
+});
+
+app.listen(PORT, () => {
+  // tslint:disable-next-line
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
