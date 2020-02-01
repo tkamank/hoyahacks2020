474900befc2213c3a601c6d451febb89161a1cac
Kirin Patel <kirinpatel@gmail.com>
2020-02-01 00:26:22 -0500

    Added ios url type and removed extra google scopes

--- mobile_app/App.tsx
+++ mobile_app/App.tsx
@@ -33,7 +33,7 @@ import { CLIENT_ID, IOS_CLIENT_ID } from 'react-native-dotenv';
 declare var global: {HermesInternal: null | {}};
 
 GoogleSignin.configure({
-  scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+  scopes: [], // what API you want to access on behalf of the user, default is email and profile
   webClientId: CLIENT_ID, // client ID of type WEB for your server (needed to verify user ID and offline access)
   offlineAccess: true, // if you want to access Google API on behalf of the user FROM YOUR SERVER
   iosClientId: IOS_CLIENT_ID, // [iOS] optional, if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
